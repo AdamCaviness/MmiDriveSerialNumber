14bc38a832d73a02a93e1b1840f43e782f1e26d4
Adam Caviness <adamcaviness@gmail.com>
2021-08-20 09:01:20 -0400

    Add initial test app

---
+++ MmiDriveSerialNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using DeviceId.Windows.Mmi.Components;
+using DeviceId.Windows.Wmi.Components;
+
+namespace MmiDriveSerialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine($"Getting disk device id...");
+
+            var wmiSw = Stopwatch.StartNew();
+			var wmiDiskDeviceId = GetWmiDeviceId();
+            Console.WriteLine($"WMI disk device id: {wmiDiskDeviceId}, took {wmiSw.ElapsedMilliseconds}ms.");
+
+			var mmiSw = Stopwatch.StartNew();
+            var mmiDiskDeviceId = GetMmiDeviceId();
+			Console.WriteLine($"MMI disk device id: {mmiDiskDeviceId}, took {mmiSw.ElapsedMilliseconds}ms.");
+        }
+
+		public static string GetMmiDeviceId()
+        {
+			return new MmiSystemDriveSerialNumberDeviceIdComponent().GetValue();
+		}
+		public static string GetWmiDeviceId()
+		{
+			return new WmiSystemDriveSerialNumberDeviceIdComponent().GetValue();
+		}
+	}
+}
